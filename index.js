50fcfb3dd09a260a842d268641696dc74bec21dc
Ben Wang <benwangott@gmail.com>
2024-05-14 00:13:12 -0400

    explore mathsteps from socratic

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+const mathsteps = require('mathsteps');
+const steps = mathsteps.solveEquation('2x + 3x = 35');
 
-app.get('/', (req, res) => res.render('index'));
-app.get('/about', (req, res) => res.render('about'));
-app.get('/algebra', (req, res) => res.render('algebra'));
-app.get('/limits', (req, res) => res.render('limits'));
-app.get('/derivatives', (req, res) => res.render('derivatives'));
+steps.forEach(step => {
+    console.log("before change: " + step.oldEquation.ascii());  // e.g. before change: 2x + 3x = 35
+    console.log("change: " + step.changeType);                  // e.g. change: SIMPLIFY_LEFT_SIDE
+    console.log("after change: " + step.newEquation.ascii());   // e.g. after change: 5x = 35
+    console.log("# of substeps: " + step.substeps.length);      // e.g. # of substeps: 2
+});
+
+app.get('/', (request, response) => response.render('index'));
+app.get('/about', (request, response) => response.render('about'));
+app.get('/algebra', (request, response) => response.render('algebra'));
+app.get('/limits', (request, response) => response.render('limits'));
+app.get('/derivatives', (request, response) => response.render('derivatives'));
 
 const port = 3000;
 app.listen(port, console.log(`Express server initialized on port ${port} `,'\n'));
